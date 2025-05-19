cda4ddc064b8fa691ecccb10152b38b56942e79e
Fran <franvi027@gmail.com>
2025-05-19 21:52:27 +0200

    pesadilla

--- index.js
+++ index.js
@@ -11,11 +11,13 @@ app.use(express.json());
 
 app.post('/contacto', async (req, res) => {
   const { nombre, email, mensaje } = req.body;
+  console.log("Datos recibidos:", nombre, email, mensaje);
 
   try {
     const serviceID = process.env.SERVICE_ID;
     const templateID = process.env.TEMPLATE_ID;
     const userID = process.env.USER_ID;
+    console.log("Variables entorno:", serviceID, templateID, userID);
 
     const data = {
       service_id: serviceID,
@@ -32,7 +34,7 @@ app.post('/contacto', async (req, res) => {
 
     res.status(200).json({ mensaje: 'Mensaje enviado correctamente' });
   } catch (error) {
-    console.error('Error al enviar el mensaje:', error.message);
+    console.error('Error al enviar el mensaje:', error.response?.data || error.message);
     res.status(500).json({ error: 'Error al enviar el mensaje' });
   }
 });
