57bfce836f343a08dc4383f1865d0c1e99417b55
Fran <franvi027@gmail.com>
2025-05-19 21:02:35 +0200

    Primer commit del microservicio

---
+++ index.js
@@ -0,0 +1,43 @@
+require('dotenv').config();  // carga variables de .env
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/contacto', async (req, res) => {
+  const { nombre, email, mensaje } = req.body;
+
+  try {
+    const serviceID = process.env.SERVICE_ID;
+    const templateID = process.env.TEMPLATE_ID;
+    const userID = process.env.USER_ID;
+
+    const data = {
+      service_id: serviceID,
+      template_id: templateID,
+      user_id: userID,
+      template_params: {
+        nombre,
+        email,
+        mensaje,
+      },
+    };
+
+    await axios.post('https://api.emailjs.com/api/v1.0/email/send', data);
+
+    res.status(200).json({ mensaje: 'Mensaje enviado correctamente' });
+  } catch (error) {
+    console.error('Error al enviar el mensaje:', error.message);
+    res.status(500).json({ error: 'Error al enviar el mensaje' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
+
